24dab3f77dce3b3e80d675134349698e399dee78
Lema05 <lemakipeno@gmail.com>
2022-10-04 17:17:53 +0300

    dictionaries in python

---
+++ dictionary.py
@@ -0,0 +1,13 @@
+person = {
+    "name": "Allen",
+    "age": 20,
+    "adress": "USA"
+}
+
+print(person["name"])
+print(person["age"])
+print(person["adress"])
+
+person["age"] = 100
+
+print(person["age"])
