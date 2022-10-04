136bc5d599c04dcef905cc6e445fcc1660c375a9
Lema05 <lemakipeno@gmail.com>
2022-10-04 16:12:16 +0300

    operators in Python

---
+++ operators.py
@@ -0,0 +1,26 @@
+#Arithmetic operators
+
+print(1 + 2)
+print(2 * 2)
+print(10 / 2)
+print(7 % 10)
+print(10 ** 5)
+
+#comparison operators
+
+print( 10 > 5)
+print( 10 >= 50)
+print(10 < 12)
+print(10 != 10)
+print(10==10)
+
+#logical operators
+print(10 > 5
+ and 1 > 3
+ and "A" == "C")
+
+print(10 < 5
+ or 1 > 3
+ or "A" == "C")
+
+print(not("Allen" == "Alley"))
